8d1d35696b4bce95f893c81e40e036879eee5849
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-09-12 20:28:00 -0700

    added fitness-controller.js

--- model/fitness.js
+++ model/fitness.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema({
+const Schema = mongoose.Schema;
+
+const exerSchema = new Schema({
   type: {
     type: String,
     trim: true,
@@ -8,4 +10,43 @@ const Schema = mongoose.Schema({
     type: String,
     trim: true,
   },
+  distance: {
+    type: Number,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    trim: true,
+  },
+  weight: {
+    type: Number,
+    trim: true,
+  },
+  reps: {
+    type: Number,
+    trim: true,
+  },
+  sets: {
+    type: Number,
+    trim: true,
+  },
 });
+
+const workSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now(),
+  },
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
+
+  // utilizing top schema
+  exercise: [exerSchema],
+});
+
+const Workout = mongoose.model("Workout", workSchema);
+
+// exerSchema > workSchema > Workout
+module.exports = { Workout };
